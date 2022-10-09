13f4207c32a52253012dcc75f194db172b59414d
Dsenpai <111998567+Dsenpai@users.noreply.github.com>
2022-10-09 18:55:32 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -4771,6 +4771,8 @@ qr, say, translate, fliptext, toletter
 
  『  *${global.BotName}*  』
  Powered by: *Fantox*
+ Modded by: *Dipanjan*
+
 
  🔰 To use any of these commands type 
  " *${prefix}<Command name>* ".
@@ -4821,6 +4823,102 @@ case '':
 break
 
 
+		
+		
+		
+case 'ihelp': case 'ih':{
+    if (isBan) return reply(mess.banned)	 			
+    if (isBanChat) return reply(mess.bangc)
+      
+ const helpmenu = `Konichiwa *${pushname}* Senpai,
+
+I am *Miku Nakano*, a bot developed by *Fantox*.
+
+🔰 My prefix is:  ${prefix}
+
+Here's the list of my Commands.
+
+
+ 
+ *━━━〈  🎆 Core 🎆  〉━━━*
+
+speak, miku, stalk, profile, help, delete, deleteall, listgc, listpc, welcome, support, repo, script 
+ 
+ *━━━〈  🎀 Owner 🎀  〉━━━*
+
+self, public, ban, bangroup, bye, join, bye, block, unblock, broadcast 
+
+ *━━━〈  ⭕ Group ⭕  〉━━━*
+ 
+promote, demote, revoke, add, remove, tagall, hidetag, groupsetting, grouplink, setgcpp, setname, setdesc, group, nsfw 
+
+ *━━━〈  ➰ Anti Link ➰  〉━━━*
+ 
+antilinkgc, antilinktg, antilinktt, antilinkytch, antilinkytvid, antilinkig, antilinkfb, antilinktwit, antilinkall, antiwame
+
+ *━━━〈  🔍 Search 🔍  〉━━━*
+
+play, ytmp3, ytmp4, yts, lyrics, google, gimage, pinterest, image, movie, wallpaper, searchgc, happymod, wikimedia, ringtone, anime, animestory, manga, ringtone  
+
+ *━━━〈  🔰 Convert 🔰  〉━━━*
+
+sticker, toimg, tovideo, togif , steal, stickermeme, emojimix, tourl, tomp3, toaudio
+
+ *━━━〈  🔉 Audio 🔉  〉━━━*
+
+bass, tempo, blown, deep, earrape, fast, fat, nightcore, reverse, robot, slow, squirrel
+
+ *━━━〈  📍 Reactions 📍  〉━━━*
+
+bonk, cry, bully, cuddle, hug, kiss, lick, pat, smug, yeet, blush, smile, wave, highfive, handhold, nom, glomp, bite, slap, kill, happy, wink, poke, dance, cringe
+
+ *━━━〈  🌌 Downloader 🌌  〉━━━*
+
+play, ytmp3, ytmp4, ytvideo, mediafire, instagram, igtv, facebook, fbmp3, twitter, twittermp3, tiktok, tiktokaudio, tiktoknowm, mediafire  
+
+ *━━━〈  🈴 Weeb 🈴  〉━━━*
+
+crosplay, waifu, loli, neko, ppcouple, feed, foxgirl, feed, meow, tickle, wallpaper, coffee, animenom, waifu3, neko2, feed, meow, tickle, migumin, awoo, animewallpaper2, anime, manga
+
+ *━━━〈  ♨️ Informative ♨️  〉━━━*
+
+animequote, quote, covid, earthquake, wiki
+
+ *━━━〈  🎗 Others 🎗  〉━━━*
+
+stickermeme, quotes, darkjoke 
+
+ *━━━〈  🎐 Fun 🎐  〉━━━*
+
+reaction, truth, dare, couple, soulmate, handsomecheck, beautifulcheck, awesomecheck, greatcheck, gaycheck, cutecheck, lesbiancheck, hornycheck, prettycheck, lovelycheck, uglycheck, charactercheck
+
+ *━━━〈  🪁 Essentials 🪁  〉━━━*
+
+qr, say, translate, fliptext, toletter
+
+ *━━━〈  💥 NSFW 💥  〉━━━*
+
+🍁 Type " *${prefix}nsfw* " then enable NSFW (Admin only!) 
+
+🍁 Then type " *${prefix}nsfwmenu* " to get full list of NSFW commands.
+
+
+
+
+ 『  *${global.BotName}*  』
+ Powered by: *Fantox*
+ Modded by: *Dipanjan*
+
+ 🔰 To use any of these commands type 
+ " *${prefix}<Command name>* ".
+ 
+ 🔰 To get Support Group link type " *${prefix}support* ".
+
+ 🔰 Type " *${prefix}help* " to get full command list.`
+ }
+ break
+ 
+ 
 
 case 'miku':
     if (isBan) return reply(mess.banned)	 			
